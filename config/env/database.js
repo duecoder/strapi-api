897d7332ac27cf4f0c363e6fb4d777dff1c6b265
duecoder <eduardodecastro@outlook.com>
2022-10-05 20:00:37 -0300

    updates

--- config/env/database.js
+++ config/env/database.js
@@ -1,7 +1,11 @@
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  const { host, port, database, user, password } = parse(
+    env(
+      "postgres://shovtuknqbjwzp:b00e08a0c01ca910b78ba74fd0acbf714530b52d14128415ae24c19cbb8dd7e7@ec2-54-86-106-48.compute-1.amazonaws.com:5432/df1pbo5ojkqv60"
+    )
+  );
 
   return {
     defaultConnection: "default",
